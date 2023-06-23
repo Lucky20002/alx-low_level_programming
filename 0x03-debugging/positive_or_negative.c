7ee6dc8b99764ea1e57db84a6c5c0ad11eccf964
Lucky20002 <luckyshabangu2000@gmail.com>
2023-06-22 20:43:58 -0700

    task

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * main - determi if a number is positive, negative or zero
+(* 0 : is the nnumber to be checked
+ * Return: 0 success
+ */
+void positive_or_negative(int i)
+{
+if (i < 0)
+{printf("%d is %s\", i, "negative");
+}
+else if (i < 0)
+{printf("%d is %s\", i, "positive");
+}
+else
+{
+printf("%d is %s\", i, "zero");
+}
+return;
+}
